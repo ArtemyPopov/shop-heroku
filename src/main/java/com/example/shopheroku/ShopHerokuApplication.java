04d8fa1d3e1956985223583a8737bb2807593830
Artem <artemy.popov95@gmail.com>
2022-04-21 15:18:30 +0500

    Initial commit

---
+++ src/main/java/com/example/shopheroku/ShopHerokuApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopheroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopHerokuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopHerokuApplication.class, args);
+    }
+
+}
